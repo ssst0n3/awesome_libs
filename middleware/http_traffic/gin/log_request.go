d67e2dd13affbd26fed654dac3385c82345655bc
st0n3 <ssst0n3@gmail.com>
2020-09-19 20:45:30 +0800

    修改：     middleware/http_traffic/gin/log_request.go

--- middleware/http_traffic/gin/log_request.go
+++ middleware/http_traffic/gin/log_request.go
@@ -10,7 +10,7 @@ import (
 	"net/http/httputil"
 )
 
-func format(c *gin.Context)  {
+func format(c *gin.Context) {
 	//log.Format(true, c.Request.Method, c.Request.URL.String(), c.Request.Header, )
 }
 
@@ -19,18 +19,16 @@ func (g Gin) LogRequest(write writer.Wrapper) gin.HandlerFunc {
 		if body, err := ioutil.ReadAll(context.Request.Body); err != nil {
 			awesome_error.CheckErr(err)
 		} else {
-			if len(body) > 0 {
-				context.Request.Body = ioutil.NopCloser(bytes.NewReader(body))
+			context.Request.Body = ioutil.NopCloser(bytes.NewReader(body))
 
-				requestDump, err := httputil.DumpRequest(context.Request, true)
-				if err != nil {
-					fmt.Println(err)
-				}
-				fmt.Println(string(requestDump))
+			requestDump, err := httputil.DumpRequest(context.Request, true)
+			if err != nil {
+				fmt.Println(err)
+			}
+			fmt.Println(string(requestDump))
 
-				if _, err := write.Write(body); err != nil {
-					awesome_error.CheckErr(err)
-				}
+			if _, err := write.Write(body); err != nil {
+				awesome_error.CheckErr(err)
 			}
 		}
 		context.Next()
