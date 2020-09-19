2f3da09a790d705eb0c83e773f50c9411efa4ac1
st0n3 <ssst0n3@gmail.com>
2020-09-19 15:38:21 +0800

    修改：     middleware/http_traffic/gin/gin.go

--- middleware/http_traffic/gin/gin.go
+++ middleware/http_traffic/gin/gin.go
@@ -1,21 +1,6 @@
 package gin
 
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
-	"github.com/ssst0n3/awesome_libs/middleware/http_traffic/log_request"
-	"github.com/ssst0n3/awesome_libs/middleware/http_traffic/log_response"
-)
-
 type Gin struct {
 }
 
 var G Gin
-
-func NewGinDefaultWriter() gin.HandlerFunc {
-	return log_request.NewGinDefaultWriter(G)
-}
-
-func NewLogrus(logger *logrus.Logger) gin.HandlerFunc {
-	return log_response.NewLogrus(G, logger)
-}
