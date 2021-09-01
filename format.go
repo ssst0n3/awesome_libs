9f544d09f8b4004a127731af873bc4f6e2081249
Lei Wang <ssst0n3@gmail.com>
2021-09-01 16:47:22 +0800

    enhance Format

--- format.go
+++ format.go
@@ -8,9 +8,14 @@ import (
 
 var t = template.New("awesome_libs").Delims("{", "}")
 
-func Format(tpl string, arg Dict) string {
+func Format(tpl string, arg Dict, delimiters ...string) string {
 	var msg bytes.Buffer
 
+	if len(delimiters) == 2 {
+		left := delimiters[0]
+		right := delimiters[1]
+		t = template.New("awesome_libs").Delims(left, right)
+	}
 	t, err := t.Parse(tpl)
 	awesome_error.CheckFatal(err)
 
